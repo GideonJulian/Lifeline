e97eb81a5f6cacd38111b11b56ae9978c38871d7
Gideon Julian <gideonchinonso77@gmail.com>
2025-12-11 21:40:50 +0100

    updated

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./layouts/Layout";
+
+const router = createBrowserRouter([  
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
+    }
+])
+
+function App() {
+    return (
+        <RouterProvider router={router} />
+     )
+ }
+ export default App;
